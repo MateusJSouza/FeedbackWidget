4113ab9d516aaad56a20ede0215164279d5d9d0b
MateusJSouza <mateusjdev@gmail.com>
2022-05-05 17:03:23 -0300

    feat: envio de e-mail com o nodemailer

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,17 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const app = express()
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "6db0fbe4d8b552",
+    pass: "4f5332cce62012"
+  }
+});
 
 // GET -> Buscar informações
 // POST -> Cadastrar informações
@@ -10,7 +20,7 @@ const app = express()
 // DELETE -> Deletar uma informação
 
 // Verificar se tem algo na requisição em formato JSON
-app.use(express.json())
+app.use(express.json());
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body
@@ -22,6 +32,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Mateus Jesus <batata@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+      `<p>Tipo do feedback: ${type} </p>`,
+      `<p>Comentário: ${comment} </p>`,
+      `</div>`
+    ].join('\n'),
   })
   
   return res.status(201).json({ data: feedback })
